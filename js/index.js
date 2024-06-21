256612c72027d4b94ba73b6ebeb0f89ca62e8eac
SilenceCr1stal <nikitapolyashov2@gmail.com>
2024-06-21 17:34:27 +0300

    added scrool triggers in main content

--- js/index.js
+++ js/index.js
@@ -7,3 +7,21 @@ ScrollSmoother.create({
   effects: true,
   
 })
+
+gsap.fromTo('.hero-section', { opacity: 1 }, {
+  opacity: 0,
+  scrollTrigger: {
+    trigger: '.hero-section',
+    start: 'center',
+    end: '1020px',
+    scrub: true
+  }
+})
+
+gsap.fromTo('.gallery__left .gallery__item', { x: -150, opacity: 0 }, {
+  opacity: 1, x: 0,
+  scrollTrigger: {
+    trigger: '.gallery__item',
+    scrub: true
+  }
+})
